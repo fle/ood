70e4d3f997769cd7481675fe5fe4e6e0e3cfbfdd
Florent Lebreton <florentlebreton@free.fr>
2014-06-19 13:53:45 +0200

    Map

--- app/js/app.js
+++ app/js/app.js
@@ -25,6 +25,23 @@ oodApp.controller('TanCtrl', ['$scope', '$http', '$timeout', function ($scope, $
 
 oodApp.controller('TrafficCtrl', ['$scope', '$http', '$timeout', function ($scope, $http, $timeout) {
 
+    /* Change section color based on fluidity */
+    var leaflet_style = function(feature) {
+      var color;
+      switch (feature.properties.Couleur_TP) {
+        case '3': color = "#00aa55"; break;
+        case '4': color = "#ffff00"; break;
+        case '5': color = "#ff9900"; break;
+        case '6': color = "#ff0000"; break;
+        default: color = "#ffffff";
+      }
+      return {
+        color: color,
+        weight: 4,
+        opacity: 1
+      }
+    }
+
     /* Base map parameters */
     angular.extend($scope, {
         center: {
@@ -44,48 +61,40 @@ oodApp.controller('TrafficCtrl', ['$scope', '$http', '$timeout', function ($scop
         angular.extend($scope, {
             geojson: {
                 data: data,
-                style: {
-                    fillColor: "green",
-                    weight: 2,
-                    opacity: 1,
-                    color: 'white',
-                    dashArray: '3',
-                    fillOpacity: 0.7
-                }
+                style: leaflet_style,
             }  
         });
-
-        $scope.geojson.style = function(feature) {
-          switch (feature.properties.Couleur_TP) {
-            case '1': return {color: "#ff0000"};
-            case '2':   return {color: "#0000ff"};
-          }
-        };
 
         refresh();
 
     });
 
-  var refresh = function() {
+    var refresh = function() {
     $http
     .get('/traffic')
     .success(function (data, status, headers, config) {
       
-      var odata = data['opendata']['answer']['data']['Troncons'];
-      $scope.timestamp = odata['Horodatage'];
+      var odata = data.opendata.answer.data.Troncons;
+      $scope.timestamp = odata.Horodatage;
 
-      var fluidity = odata['Troncon'];
-      var sections = $scope.geojson.data.features;
-      for(var i=0; i<fluidity.length; i++){
+      var fluidity = odata.Troncon;
+      var sections = $scope.geojson.data;
+      for(var i=0; i < fluidity.length; i++){
         var item = fluidity[i];
-        for(var j=0; j<sections.length; j++){
-          var section = sections[j];
-          if(item['Id'] == section['properties']['ID']) {
-            section.Couleur_TP = item['Couleur_TP'];
+        for(var j=0; j < sections.features.length; j++){
+          var section = sections.features[j];
+          if(item.Id == section.properties.ID) {
+            sections.features[j].properties.Couleur_TP = item.Couleur_TP;
             continue;
           }
         }
       }
+
+      $scope.geojson = {};
+      $scope.geojson = {
+        data: sections,
+        style: leaflet_style
+      };
 
     });
 
